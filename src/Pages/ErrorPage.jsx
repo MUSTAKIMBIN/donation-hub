4ddd2cc2186801ce5acaf604073f45723227bc6d
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-10-27 07:23:09 +0600

    updated ErrorPage

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            Ops!!
+        <div className='flex flex-col justify-center items-center h-[50vh]'>
+            <div className='text-center'>
+                <p>Oopss! </p>
+                <p>Somethin went wrong</p>
+            </div>
+            <div>
             <Link to='/'><button>Home</button></Link>
+            </div>
         </div>
     );
 };
