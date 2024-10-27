f4a10ad0152e438a5d683c92022e654e3622a272
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-10-27 06:54:12 +0600

    create basic folder structure and route set up

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Hello EveryOne</h2>,
+  }
+]);
+export default router;
